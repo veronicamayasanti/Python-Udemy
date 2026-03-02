141b7fa73628fff7029f2237178efb4c8c1dddbd
Veronica Maya Santi <veronicamayasanti@gmail.com>
2026-03-02 19:43:30 +0700

    string

---
+++ python-dasar/manipulasi_string.py
@@ -0,0 +1,12 @@
+nama = "maya"
+umur = 34
+
+pesan = "nama saya " + nama + ", umur saya " + str(umur)
+
+print(pesan)
+print(len(pesan))
+
+kota = "bogor raya"
+print(kota[0])
+# melihat karater dari belakang menggunakan -
+print(kota[-1])
---
+++ python-dasar/variabel.py
@@ -0,0 +1,39 @@
+nama = "maya"
+umur = 25
+tinggi = 150.3
+
+# cara menulis string bisa menggunakan petik 1 petik 2 dan petik 3 untuk multi line
+puisi = """
+ini adalah puisi
+yang lebih dari
+satu baris
+"""
+
+# Boolean values
+# dalam penuliasn True dan False huruf depan T dan F harus kapital python case sensitive
+
+is_married = True
+
+# Multiple assignment
+x = y = z = 0
+a, b, c = 1,2,3
+name, age = "tata", 8
+
+print(nama)
+print(umur)
+print(tinggi)
+print(type(nama))
+print(type(tinggi))
+print(type(umur))
+print(type(puisi))
+print(type(is_married))
+print(type(name))
+print(type(age))
+print(x,y,z)
+print(a,b,c)
+print(name,age)
+
+skor = 80
+print(skor)
+skor = 90
+print(skor)
