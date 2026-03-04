82022758445d2ca27f3e5fc059f692caf5dd648b
Veronica Maya Santi <veronicamayasanti@gmail.com>
2026-03-04 12:00:49 +0700

    operator python

---
+++ python-dasar/kontrol-alur.py
@@ -0,0 +1,13 @@
+# kontrol alur program adalah cara untuk  mengatur jalannya program berdasarkan kondisi tertentu
+# dengan kontrol alur, program bisa mengambil keputusan dan melakukan tindakan yang berbeda tergantung pada situasi
+
+# penyataan if
+# pernyataan if digunakan untuk menjalankan kode hanya jika kondisi tertentu bernilai True
+# setelah if, harus ada : (titik dua)
+# kode yang akan dijalankan jika kondisi True harus diindetasi (diberi spasi di awal)
+
+angka = int(input("masukan angka : "))
+
+if angka > 0 :
+    print("angka positif")
+
---
+++ python-dasar/operator.py
@@ -0,0 +1,39 @@
+# operator aritmatika
+a = 10
+b = 3
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(10%2)
+print(11%2)
+
+c = 2
+d = 3
+print(c ** d)
+
+print(5 ** 2)
+
+# operator assignment (penugasan)
+x = 10
+print("x awal:", x)
+x += 5 # sama dengan x = x + 5
+print("x setelah x +=5 : ", x)
+x -= 3 # sama dengan x = x -3
+print(" x setelah -= 3 : ", x)
+x *= 2 # sama dengan : x = x*2
+print("setelah *=2: ", x)
+x /= 4
+print("x setelah /= 4: ", x)
+
+# operator string
+# repetition (*)
+garis = " - "
+print(garis * 20)
+# membership (in), mengecek apakah text ada dalam string
+kalimat = "Python adalah bahasa pemrograman"
+print("Python" in kalimat)
+print("Java" in kalimat)
